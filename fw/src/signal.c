602bb800d04da8d34c48c73d77f8acd03b272777
Jan Horacek <me@apophis.cz>
2024-07-04 22:14:03 +0200

    Do not write flashing output to first item in record.

--- fw/src/signal.c
+++ fw/src/signal.c
@@ -7,7 +7,7 @@
 #include "pwm.h"
 
 typedef struct {
-	uint8_t outputs; // mask, see leds.h
+	uint8_t permanent; // mask, see leds.h
 	uint8_t flash; // mask, see leds.h
 } SignalCode;
 
@@ -17,38 +17,38 @@ const SignalCode codes[2][NO_CODES] PROGMEM = {{
 	{LED_RED, 0},
 	{LED_GREEN, 0},
 	{LED_YELLOW_TOP, 0},
-	{LED_YELLOW_TOP, LED_YELLOW_TOP},
+	{0, LED_YELLOW_TOP},
 	{LED_GREEN | LED_YELLOW_BOTTOM, 0},
 	{LED_ALL, 0},
 	{LED_YELLOW_TOP | LED_YELLOW_BOTTOM, 0},
-	{LED_YELLOW_TOP | LED_YELLOW_BOTTOM, LED_YELLOW_TOP},
-	{LED_RED | LED_WHITE, LED_WHITE}, // PN
+	{LED_YELLOW_BOTTOM, LED_YELLOW_TOP},
+	{LED_RED, LED_WHITE}, // PN
 	{LED_WHITE, 0},
 	{LED_RED | LED_WHITE, 0},
 	{LED_GREEN | LED_WHITE, 0},
 	{LED_YELLOW_TOP | LED_WHITE, 0},
 	{0, 0},
-	{LED_YELLOW_TOP | LED_WHITE, LED_YELLOW_TOP},
+	{LED_WHITE, LED_YELLOW_TOP},
 	{LED_YELLOW_TOP | LED_WHITE | LED_YELLOW_BOTTOM, 0},
-	{LED_YELLOW_TOP | LED_WHITE | LED_YELLOW_BOTTOM, LED_YELLOW_TOP},
+	{LED_WHITE | LED_YELLOW_BOTTOM, LED_YELLOW_TOP},
 }, {
 	{LED_WHITE | LED_YELLOW_TOP, 0},
 	{LED_WHITE | LED_GREEN, 0},
 	{LED_WHITE | LED_GREEN, 0},
 	{LED_WHITE | LED_GREEN, 0},
-	{LED_WHITE | LED_YELLOW_TOP, LED_YELLOW_TOP},
+	{LED_WHITE, LED_YELLOW_TOP},
 	{LED_ALL, 0},
-	{LED_WHITE | LED_YELLOW_TOP, LED_YELLOW_TOP},
-	{LED_WHITE | LED_YELLOW_TOP, LED_YELLOW_TOP},
+	{LED_WHITE, LED_YELLOW_TOP},
+	{LED_WHITE, LED_YELLOW_TOP},
 	{LED_WHITE | LED_YELLOW_TOP, 0}, // PN
 	{LED_WHITE | LED_YELLOW_TOP, 0},
 	{LED_WHITE | LED_YELLOW_TOP, 0},
 	{LED_WHITE | LED_GREEN, 0},
 	{LED_WHITE | LED_YELLOW_TOP, 0},
 	{0, 0},
-	{LED_WHITE | LED_YELLOW_TOP, LED_YELLOW_TOP},
+	{LED_WHITE, LED_YELLOW_TOP},
 	{LED_WHITE | LED_YELLOW_TOP, 0},
-	{LED_WHITE | LED_YELLOW_TOP, LED_YELLOW_TOP},
+	{LED_WHITE, LED_YELLOW_TOP},
 }};
 
 volatile int8_t current_signal_code = -1;
@@ -66,15 +66,17 @@ void set_signal_code(uint8_t code) {
 	if (code >= NO_CODES)
 		return;
 
-	uint8_t turn_on = signal_code(code).outputs;
+	uint8_t code_all = signal_code(code).permanent | signal_code(code).flash;
+	uint8_t current_all = signal_code(current_signal_code).permanent | signal_code(current_signal_code).flash;
+	uint8_t turn_on = code_all;
 
 	if (current_signal_code != -1) {
-		turn_on &= ~signal_code(current_signal_code).outputs;
-		uint8_t turn_off = signal_code(current_signal_code).outputs & (~signal_code(code).outputs);
-		if (!flash_state) {
-			// turn on all outputs to be turned on in case of flashing off
-			turn_on |= signal_code(code).outputs & signal_code(current_signal_code).flash;
-			turn_off &= ~(signal_code(current_signal_code).flash); // do not turn off already turned off
+		turn_on &= ~signal_code(current_signal_code).permanent;
+		uint8_t turn_off = current_all & (~code_all);
+		if (flash_state) {
+			turn_on &= ~signal_code(current_signal_code).flash; // do not turn on already turned on
+		} else {
+			turn_off &= ~signal_code(current_signal_code).flash; // do not turn off already turned off
 		}
 		ramp_down(turn_off);
 	}
